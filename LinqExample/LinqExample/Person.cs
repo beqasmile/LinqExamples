74976061d244d36584817a92e7d98144f9161a70
ilya <beqasmile@gmail.com>
2020-06-03 19:43:13 +0300

    LinQ

---
+++ LinqExample/LinqExample/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string id;
+       
+        public Person(string firstName, string lastName, int age)
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age { get => age; set => age = value; }
+        public string Id { get => id; set => id = value; }
+    }
+}
---
+++ LinqExample/LinqExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person pers1 = new Person("Moshe", "Davidov", 15);
+            Person pers2 = new Person("Dany", "Davidov", 2);
+            Person pers3 = new Person("Alex", "Davidov", 22);
+            Person pers4 = new Person("Rahel", "Davidov", 55);
+
+            List<Person> people = new List<Person>();
+            people.Add(pers1);
+            people.Add(pers2);
+            people.Add(pers3);
+            people.Add(pers4);
+
+
+            List<Person> peopleDavidov = people.Where(x => x.LastName == "Davidov").ToList();
+            List<Person> peopleBigger = people.Where(x => x.Age > 10).ToList();
+            List<Person> peopleBiggerDavidov = people.Where(x => x.Age > 10 && x.LastName=="Davidov").ToList();
+            Dictionary<string, Person> dictpeopleBiggerDavidov = people.Where(x => x.Age > 10 && x.LastName == "Davidov").ToDictionary(x=>x.Id);
+
+
+
+        }
+    }
+}
